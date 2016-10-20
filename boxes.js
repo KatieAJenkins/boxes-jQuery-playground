98971a25b8c66c4b7f07cae2cf4d3cdd89a27748
KatieAJenkins <kind2karma@gmail.com>
2016-10-19 18:20:46 -0600

    initial commit

--- boxes.js
+++ boxes.js
@@ -1,1 +1,18 @@
-//write your code here
+
+"use strict"//write your code here
+  $( document ).ready(function() {
+      // console.log( "document loaded" );
+      //
+      // console.log($("#secretBox").css("background-color", "white"));
+      //
+      // $("#secretBox").append("<h1>secret box!</h1>");
+      //
+      // console.log($(".boxType1").css("background-color" , "white"));
+
+    // console.log( $("#row1").children().addClass("boxType3"));
+
+    // console.log($(".boxType1").last().hide());
+
+  console.log($("#row2").remove( ":contains('boxType')"));
+
+  });
